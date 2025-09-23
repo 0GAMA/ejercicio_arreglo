c97582864bba6efe8128713e5d1cc29bf1e6291f
0GAMA <polop1230@gmail.com>
2025-09-23 12:09:56 -0500

    se definen los arreglos vacios y funciones del proyecto

---
+++ script.js
@@ -0,0 +1,21 @@
+let nombresEstudiantes = []
+let documentosEstudiantes = []
+let notasEstudiantes = []
+
+let registrarEstudiante = () => {
+
+}
+
+let buscarEstudiante = () => {
+
+}
+let filtrarEstudiantesGanaron = () => {
+
+}
+let filtrarEstudiantesPerdieron = () => {
+
+}
+
+let filtraEstudiantesConPlanMejoramiento = () => {
+
+}
