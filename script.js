7790aff39563c1111f54c8602e6240b9aabe2c6b
0GAMA <polop1230@gmail.com>
2025-09-23 13:10:13 -0500

    Creacion de el menu de opciones e invoca la funcion de refistro de estudiantes

--- script.js
+++ script.js
@@ -23,5 +23,36 @@ let registrarEstudiante = () => {
 let buscarEstudiante = () => { };
 let filtrarEstudiantesGanaron = () => { };
 let filtrarEstudiantesPerdieron = () => { };
-
 let filtraEstudiantesConPlanMejoramiento = () => { };
+
+let repetir = false
+
+while (repetir) {
+    let opcion = prompt(
+        "Selecciones una opcion:\n 1 = Registrar estudiante\n 2 = Buscar estudiante\n 3 = Filtrar estudiantes ganadores\n 4 = Filtrar estudiantes perdedores\n 5 = Filtrar estudiantes con plan de mejoramiento\n 6 = Salir"
+    );
+    switch (opcion) {
+        case "1":
+            registrarEstudiante();
+            break;
+        case "2":
+            buscarEstudiante();
+            break;
+        case "3":
+            filtrarEstudiantesGanaron();
+            break;
+        case "4":
+            filtrarEstudiantesPerdieron();
+            break;
+        case "5":
+            filtraEstudiantesConPlanMejoramiento();
+            break;
+        case "6":
+            repetir = false;
+            break;
+        default:
+            alert("Opción inválida");
+            break;
+    }
+}
+
