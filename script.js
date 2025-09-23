9849981fcfe725f61de3c862e964e0ce79909ea1
0GAMA <polop1230@gmail.com>
2025-09-23 12:54:12 -0500

    se finaliza funcion de registrar estudiantes

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let nombresEstudiantes = []
-let documentosEstudiantes = []
-let notasEstudiantes = []
+let nombresEstudiantes = [];
+let documentosEstudiantes = [];
+let notasEstudiantes = [];
 
 let registrarEstudiante = () => {
-
+    let repetir = true;
+    while (repetir) {
+        let nombresEstudiantes = prompt("Ingrese el nombre del estudiante: ");
+        let documentosEstudiantes = prompt("Ingrese el documento del estudiante: ");
+        let notasEstudiantes = prompt("Ingrese la nota del estudiante: ");
+        nombresEstudiantes.push(nombresEstudiantes);
+        documentosEstudiantes.push(documentosEstudiantes);
+        notasEstudiantes.push(notasEstudiantes);
+        let opcion = prompt(
+            "¿Desea registrar un nuevo estudiante?\n 1 = Si\n 2 = No "
+        );
+        if (opcion == "2") {
+            repetir = false;
+        }
+    }
 }
 
-let buscarEstudiante = () => {
+let buscarEstudiante = () => { };
+let filtrarEstudiantesGanaron = () => { };
+let filtrarEstudiantesPerdieron = () => { };
 
-}
-let filtrarEstudiantesGanaron = () => {
-
-}
-let filtrarEstudiantesPerdieron = () => {
-
-}
-
-let filtraEstudiantesConPlanMejoramiento = () => {
-
-}
+let filtraEstudiantesConPlanMejoramiento = () => { };
